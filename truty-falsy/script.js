a44c077dcd1c37181a5da5bc1f0d6ae9caa9c786
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-02-04 02:43:06 +0700

    truty dan falsy

---
+++ truty-falsy/script.js
@@ -0,0 +1,12 @@
+// Truty dan Falsy
+
+// Contoh data falsy ialah: "", ``, '', false, null, undefined, NaN
+// Contoh data truty selain data falsy
+
+const data = 'ss'
+
+if(data){
+    console.log("True")
+}else{
+    console.log("False")
+}
