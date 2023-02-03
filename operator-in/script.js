b2580f38b23dca0f08134329fe09182ae584e64b
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-02-04 02:04:00 +0700

    Operator in

---
+++ operator-in/script.js
@@ -0,0 +1,9 @@
+// Menggunakan operator in untuk mengecek apakah ada properties didalam array atau object
+
+const names = {
+    nama : "Farhan",
+    umur : 22
+}
+
+console.log("nama" in names)
+console.log("kata" in names)
