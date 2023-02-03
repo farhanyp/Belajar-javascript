0c1171578162c3ec095536937cfacc9153b80dd2
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-02-04 02:47:30 +0700

    Logika non boolean

---
+++ logika-non-boolean/script.js
@@ -0,0 +1,9 @@
+// Logika non boolean
+
+// salah satu contoh dari logika OR non boolean
+// Logika OR non boolean mengambil truty untuk pertama kali
+console.log('' || "abc")
+
+// salah satu contoh dari logika AND non boolean
+// Logika AND non boolean mengambil falsy untuk pertama kali
+console.log('' && "abc")
