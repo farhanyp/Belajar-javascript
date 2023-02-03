1785985d7d5a5903a0d2081d706cba0773c9d196
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-02-04 01:58:16 +0700

    Belajar nullisih

---
+++ operator-nullish/script.js
@@ -0,0 +1,8 @@
+// Memakai nullish operator
+
+let data;
+
+// Mengecek apakah datanya null atau undifiend, jika null atau undifiend maka akan diganti
+let checkTypeData = data ?? "ini data default"
+
+console.log (checkTypeData)
