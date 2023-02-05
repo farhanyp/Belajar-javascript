f778a4205fc9d9ddba522eeb7d2b5e393b8a418b
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-02-06 02:54:22 +0700

    function didalam function

---
+++ function-didalam-function/script.js
@@ -0,0 +1,17 @@
+// Function didalam function
+
+function say(){
+    function sayMama(){
+        console.log("mama")
+    }
+
+    function sayPapa(){
+        console.log("papa")
+    }
+
+    sayMama()
+    sayPapa()
+
+}
+
+say()
