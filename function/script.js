ecf801e9df45fb66f693bd1c9e5ac9e25855c8a4
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-02-05 21:18:23 +0700

    function

--- function/script.js
+++ function/script.js
@@ -13,7 +13,22 @@ function sum(value1, value2){
     console.log(`${value1} + ${value2} = ${value1 + value2}`)
 }
 
+// Function pakai optional parameter
+function name(firstName, middleName, lastName){
+    console.log(`hai ${firstName} `)
+    console.log(`hai ${middleName} `)
+    console.log(`hai ${lastName} `)
+}
+
+// Function pakai default parameter
+function animals(firstAnimal, middleAnimal = "Koala"){
+    console.log(`this is ${firstAnimal} `)
+    console.log(`this is ${middleAnimal} `)
+}
+
 
 sayHello()
 sayHello2("Farhan")
 sum(2, 2)
+name("farhan")
+animals()
