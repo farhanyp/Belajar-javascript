a9ec7a723d30e60b6936465d3d2021bd338e6ef0
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-02-07 03:42:46 +0700

    Arrow Function

---
+++ Arrow-function/script.js
@@ -0,0 +1,15 @@
+// Arrow Function pakai parameter dan block
+// const sayHello = (name) =>{
+//     console.log(name)
+// }
+
+// Arrow Function pakai parameter tanpa pakai block
+// const sayHello = (name) => console.log(name)
+
+// Arrow Function tanpa parametern dan block
+const sayHello = name => console.log(name)
+
+
+
+
+sayHello("farhan")
