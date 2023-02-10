9659066a787d6e9164dabe6515f0331e3a3c0b4b
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-02-11 03:08:04 +0700

    menambahkan method

--- oop/class/script.js
+++ oop/class/script.js
@@ -5,7 +5,12 @@ class Person{
         // Menambahkan Property
         this.name = name
     }
+
+    // Menambahkan method
+    sayHello(name){
+        console.log(`Hai ${name} my name is ${this.name}`)
+    }
 }
 
 const yp = new Person("YP")
-console.log(yp.name)
+yp.sayHello("budy")
