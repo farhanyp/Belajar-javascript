51ecd343b6fb5bb4a2c06c8d5178b550e39d86bb
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-02-11 07:39:47 +0700

    public class field

---
+++ oop/public-class-field/script.js
@@ -0,0 +1,15 @@
+// Membuat public class field
+
+class Person{
+    firstname
+    lastname
+
+    constructor(name1, name2){
+        this.firstname = name1
+        this.lastname = name2
+    }
+}
+
+const yp = new Person("farhan", "Yudha")
+console.log(yp.firstname)
+console.log(yp.lastname)
