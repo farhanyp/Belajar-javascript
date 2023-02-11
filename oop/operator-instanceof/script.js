4282fc8e33a2a017346a943db1aaea2464d3ee5f
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-02-11 16:18:33 +0700

    Static Method

--- oop/operator-instanceof/script.js
+++ oop/operator-instanceof/script.js
@@ -1,22 +1,19 @@
-// Menggunakan instanceof untuk mengecek instance dari class mana
+// Menggunakan static method
+// Static method biasanya digunakan sebagai utility
 
-class Animals{
+
+class Math{
+
+    static sum(...numbers){
+        let total = 0
+
+        for(const number of numbers){
+            total += number
+        }
+
+        return total
+    }
 
 }
 
-class Tree{
-
-}
-
-class Human extends Tree{
-
-
-}
-
-const dog = new Animals()
-const farhan = new Human()
-const pohon = new Human()
-
-console.log(dog instanceof Animals) // True
-console.log(farhan instanceof Animals) // False
-console.log(pohon instanceof Human) // True karena child dari Tree
+console.log(Math.sum(1,1,1))
