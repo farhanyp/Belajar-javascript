9ebe6e4a8dba51eef6901dd335755034290a6d0f
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-02-21 16:16:08 +0700

    Date

--- library/Date/script.js
+++ library/Date/script.js
@@ -1,31 +1,44 @@
 // Materi Date
 
 {
-// Create Date
-// Membuat date saat ini
-console.log(new Date)
+// // Create Date
+// // Membuat date saat ini
+// console.log(new Date)
 
-// Membuat date dengan tanggal
-console.log(new Date(2023, 2, 21))
+// // Membuat date dengan tanggal
+// console.log(new Date(2023, 2, 21))
 
-// Membuat date dengan parameter sampai mili second
-console.log (new Date(2023, 2, 21, 5, 21, 30, 200))
+// // Membuat date dengan parameter sampai mili second
+// console.log (new Date(2023, 2, 21, 5, 21, 30, 200))
 
-// Membuat date dari epoch atau unix timestamp
-console.log(new Date(1676969124))
+// // Membuat date dari epoch atau unix timestamp
+// console.log(new Date(1676969124))
 }
 
 {
-    // Create epoch & unix timestamps
-    
-    // konversi format date saat ini menghasilkan timestamps
-    const timeNow = new Date()
-    console.log(timeNow.getTime())
+    // // Create epoch & unix timestamps
 
+    // // konversi format date saat ini menghasilkan timestamps
+    // const timeNow = new Date()
+    // console.log(timeNow.getTime())
+
+    // // konversi dari timestamps ke format date
+    // console.log(new Date(timeNow))
+
+    // // Mengambil date saat ini dengan keluarn timestamps
+    // console.log(Date.now())
+    
+}
+
+{
+    // Parsing data
+    // konversi dari string date ke timestamps
+    const timeString = "2023-2-21"
+    const parseTimestamp = Date.parse(timeString)
+
+    console.log(parseTimestamp)
+    
     // konversi dari timestamps ke format date
-    console.log(new Date(timeNow))
-
-    // Mengambil date saat ini dengan keluarn timestamps
-    console.log(Date.now())
+    console.log(new Date(parseTimestamp))
     
-    }
+}
