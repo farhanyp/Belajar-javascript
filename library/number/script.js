daede50b0a993ecefb241920a15a03c0c374384e
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-02-21 07:58:26 +0700

    ss

--- library/number/script.js
+++ library/number/script.js
@@ -1,13 +1,18 @@
-// Contoh penggunaan String
+// Contoh penggunaan NUMBER
 
 
-const name = "Farhan Yudha Pratama"
-console.log(name)
+let string_number = "12345"
+let number = Number(string_number)
+console.log(string_number)
+console.log(number)
 
-// Instance Properties pada number
-console.log(name.length)
+// Static Property pada number
+console.log(Number.MAX_SAFE_INTEGER)
+console.log(Number.MIN_VALUE)
+
+// Static Method pada number
+console.log(Number.isNaN(string_number))
+console.log(Number.isInteger(number))
 
 // Instance Method pada number
-console.log(name.toLowerCase())
-console.log(name.toUpperCase())
-console.log(name.split(" "))
+console.log(number.toString(2))
