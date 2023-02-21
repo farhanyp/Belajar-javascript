9b07ea65c8865ee8c6b65f09f8d351e6e1a20e60
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-02-21 08:32:42 +0700

    Array Search

--- library/Array/script.js
+++ library/Array/script.js
@@ -27,13 +27,23 @@
 
 
 // Pengunaan Stack
-const name = []
+// const name = []
 
-name.push("farhan")
-name.push("Yudha")
-name.push("Pratama")
+// name.push("farhan")
+// name.push("Yudha")
+// name.push("Pratama")
 
-console.log(name.pop())
-console.log(name.pop())
-console.log(name.pop())
-console.log(name.pop())
+// console.log(name.pop())
+// console.log(name.pop())
+// console.log(name.pop())
+// console.log(name.pop())
+
+
+// Penggunaan Array Search
+// const alfabet = ['a','b','c','d','e','a','b','c','d','e']
+
+// console.log(alfabet.find(value => value === 'd')) // mencari data berdasarkan kondisi
+// console.log(alfabet.findIndex(value => value === 'd')) // mencari index berdasarkan kondisi
+// console.log(alfabet.includes("e")) // mencari apakah terdapat data
+// console.log(alfabet.indexOf("e")) // mencari apakah terdapat data
+// console.log(alfabet.lastIndexOf("a")) // mencari apakah terdapat data
