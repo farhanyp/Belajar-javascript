5314b15f18c5d905306cabc4960b61c53a2103cf
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-02-21 08:20:17 +0700

    Queue

--- library/Array/script.js
+++ library/Array/script.js
@@ -1,13 +1,29 @@
-// Contoh penggunaan Array
+// // Contoh penggunaan Array
+// const name = ["farhan", "yudha", "pratama"]
 
-const name = ["farhan", "yudha", "pratama"]
+// // menggunakan anonymous function
+// name.forEach(function(value, index){
+//     console.log(`Index ke ${index}, dengan isi ${value}`)
+// });
 
-// menggunakan anonymous function
-name.forEach(function(value, index){
-    console.log(`Index ke ${index}, dengan isi ${value}`)
-});
+// // Menggunakan arrow function
+// name.forEach((value,index) => console.log(`Index ke ${index}, dengan isi ${value}`))
 
-// Menggunakan arrow function
-name.forEach((value,index) => console.log(`Index ke ${index}, dengan isi ${value}`))
+// name.forEach((value) => console.log(`dengan isi ${value}`))
 
-name.forEach((value) => console.log(`dengan isi ${value}`))
+
+
+// // Penggunaan Queue
+// const name = []
+
+// name.push("farhan")
+// name.push("Yudha")
+// name.push("Pratama")
+
+// console.log(name.shift())
+// console.log(name.shift())
+// console.log(name.shift())
+// console.log(name.shift())
+
+
+// Pengunaan Stack
