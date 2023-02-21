3c4d5e2a44da897e72784f15e5f22e7ea27abd32
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-02-21 08:36:52 +0700

    Array filter

--- library/Array/script.js
+++ library/Array/script.js
@@ -47,3 +47,15 @@
 // console.log(alfabet.includes("e")) // mencari apakah terdapat data
 // console.log(alfabet.indexOf("e")) // mencari apakah terdapat data
 // console.log(alfabet.lastIndexOf("a")) // mencari apakah terdapat data
+
+
+
+// Array Filter
+
+const numbers = [1,2,3,4,5,6,7,8,9,10]
+
+const ganjil = numbers.filter(value => value % 2 == 1)
+const genap = numbers.filter(value => value % 2 == 0)
+
+console.log(ganjil)
+console.log(genap)
