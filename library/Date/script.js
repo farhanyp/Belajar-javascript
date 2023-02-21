d6193d4f05ecd508239c7c62d2178b329f8de6d7
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-02-21 15:46:16 +0700

    membuat date

---
+++ library/Date/script.js
@@ -0,0 +1,13 @@
+// Materi Date
+
+// Membuat date saat ini
+console.log(new Date)
+
+// Membuat date dengan tanggal
+console.log(new Date(2023, 2, 21))
+
+// Membuat date dengan parameter sampai mili second
+console.log (new Date(2023, 2, 21, 5, 21, 30, 200))
+
+// Membuat date dari epoch atau unix timestamp
+console.log(new Date(1676969124))
