ff83bf11e5d38ad9effcb7fe711749045c9e7dd9
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-02-21 15:54:59 +0700

    Date

--- library/Date/script.js
+++ library/Date/script.js
@@ -1,5 +1,7 @@
 // Materi Date
 
+{
+// Create Date
 // Membuat date saat ini
 console.log(new Date)
 
@@ -11,3 +13,19 @@ console.log (new Date(2023, 2, 21, 5, 21, 30, 200))
 
 // Membuat date dari epoch atau unix timestamp
 console.log(new Date(1676969124))
+}
+
+{
+    // Create epoch & unix timestamps
+    
+    // konversi format date saat ini menghasilkan timestamps
+    const timeNow = new Date()
+    console.log(timeNow.getTime())
+
+    // konversi dari timestamps ke format date
+    console.log(new Date(timeNow))
+
+    // Mengambil date saat ini dengan keluarn timestamps
+    console.log(Date.now())
+    
+    }
