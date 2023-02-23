52500b187ffa6e49bba9f2d0e6294e0ea9998af4
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-02-23 07:22:27 +0700

    Math

--- library/Math/script.js
+++ library/Math/script.js
@@ -1,12 +1,12 @@
 // Materi Math
 
-{
-    const numbers = [-1,10,33,4,7,8,222]
+// {
+//     const numbers = [-1,10,33,4,7,8,222]
 
-    console.log(Math.max(numbers))
-    console.log(Math.min(numbers))
-    console.log(Math.round(10.8))
-    console.log(Math.round(10.3))
-    console.log(Math.floor(9.8))
-    console.log(Math.floor(9.3))
-}
+//     console.log(Math.max(numbers))
+//     console.log(Math.min(numbers))
+//     console.log(Math.round(10.8))
+//     console.log(Math.round(10.3))
+//     console.log(Math.floor(9.8))
+//     console.log(Math.floor(9.3))
+// }
