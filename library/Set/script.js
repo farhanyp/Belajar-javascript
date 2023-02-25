6668e4cd66cee10062aac3f86bbe38ef21cd0d73
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-02-25 08:16:33 +0700

    ss

--- library/Set/script.js
+++ library/Set/script.js
@@ -1,20 +1,20 @@
-// Materi Set
+// // Materi Set
 
-const set = new Set()
-// menset isi dari map
-set.add("Farhan Yudha Pratama")
-set.add("Farhan Yp")
+// const set = new Set()
+// // menset isi dari map
+// set.add("Farhan Yudha Pratama")
+// set.add("Farhan Yp")
 
-// Mengiterasi map
-set.forEach((value) => console.log(`value:${value}`))
+// // Mengiterasi map
+// set.forEach((value) => console.log(`value:${value}`))
 
-// Cek banyak data di map
-console.log(set.size)
+// // Cek banyak data di map
+// console.log(set.size)
 
-// Cek apakah map mempunyai data berdasarkan key
-console.log(set.has("Farhan Yudha Pratama"))
+// // Cek apakah map mempunyai data berdasarkan key
+// console.log(set.has("Farhan Yudha Pratama"))
 
-// Mendelete data map berdasarkan key
-set.delete("Farhan Yp")
-set.forEach((value) => console.log(`value:${value}`))
+// // Mendelete data map berdasarkan key
+// set.delete("Farhan Yp")
+// set.forEach((value) => console.log(`value:${value}`))
 
