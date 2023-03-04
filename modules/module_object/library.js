6aa3c4881c7793409f16427076ea8b4637122304
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-03-04 15:37:29 +0700

    object module

---
+++ modules/module_object/library.js
@@ -0,0 +1,6 @@
+// Export
+
+export const firstname = "Farhan"
+export const middlename = "Yudha"
+export const lastname = "Pratama"
+
---
+++ modules/module_object/script.js
@@ -0,0 +1,5 @@
+// Import
+// Mengimport module dan dijadikan object
+import * as Yp from './library.js'
+
+console.log(`${Yp.firstname} ${Yp.middlename} ${Yp.lastname}`)
