da1df0ce7608068898cfd157bc7bd1ce7db3a20e
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-03-04 09:40:35 +0700

    Alias

---
+++ modules/alias/library.js
@@ -0,0 +1,14 @@
+// Export class
+
+const name = "Farhan Yudha Pratama"
+
+function addition(){
+
+}
+
+class Person{
+
+}
+
+// Multiple Export
+export{name, Person, addition}
---
+++ modules/alias/script.js
@@ -0,0 +1,10 @@
+// Import
+
+import {name, addition, Person} from "./library.js"
+
+// Alias di Import
+import {name as nama, addition as tambah, Person as Orang} from "./library2.js"
+
+const person = new Person("farhan")
+const person2 = new Orang("farhan")
+
