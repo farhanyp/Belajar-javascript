35e9fc7787c3d913cc00420777d6ce4a420f66f1
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-03-06 04:55:16 +0700

    Document

---
+++ DOM/Document/script.js
@@ -0,0 +1,10 @@
+// Document Property
+// console.log(document)
+// console.log(document.title)
+// console.log(document.head)
+// console.log(document.body)
+// console.log(document.images)
+
+// Document Methods
+let text = document.getElementById('text')
+console.log(text)
