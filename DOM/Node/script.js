35def7c2c3ccdc2366ee764fa265deeb377934a1
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-03-06 05:28:39 +0700

    Node Methods

---
+++ DOM/Node/script.js
@@ -0,0 +1,18 @@
+// Node
+// let menu = document.getElementById('menu')
+// Outputnya Berupa Node
+// console.log(menu)
+
+// Node Property
+// let second = document.getElementById('second')
+// console.log(second.previousSibling.previousSibling)
+// console.log(second.nextSibling.nextSibling)
+
+// Node Methods
+// let menu = document.getElementById('menu')
+// let first = document.getElementById('first')
+// let second = document.getElementById('second')
+// let third = document.getElementById('third')
+// menu.removeChild(first)
+// menu.removeChild(second)
+// menu.removeChild(third)
