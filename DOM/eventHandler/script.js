d0bc833879ad9897b04c2ae6397664f192555baa
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-03-09 10:38:38 +0700

    Event Handler

---
+++ DOM/eventHandler/script.js
@@ -0,0 +1,14 @@
+// Event Handler
+
+const button = document.getElementById('button')
+
+// Event Handler
+// button.addEventListener('click', function(){
+//     button.setAttribute('value', 'you already click me')
+// })
+
+
+// Event Handler Global
+// button.onclick = function(){
+//     button.setAttribute('value','you already click me')
+// }
