68ed3c8981bb62a304a05b8e0614cc93b5b4a907
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-03-10 09:14:30 +0700

    inner

---
+++ DOM/Inner/script.js
@@ -0,0 +1,13 @@
+// Event
+
+const content = document.getElementById('content')
+
+// Perbedaan saat mengambil data
+// console.log(content.textContent)
+// console.log(content.innerText)
+// console.log(content.innerHTML)
+
+// Perbedaan saat menampilkan data
+// content.textContent = "<p> ini text content </p>"
+// content.innerText = "<p> ini text content </p>"
+// content.innerHTML = "<p> ini text content </p>"
