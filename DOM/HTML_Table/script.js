2d600ecd8aff38b554101aadbb915bff17a40e54
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-03-11 17:58:26 +0700

    Html Table

---
+++ DOM/HTML_Table/script.js
@@ -0,0 +1,8 @@
+// HTML Table
+
+const orderRevenue = document.getElementById('orderRevenue')
+
+console.log(orderRevenue.caption)
+console.log(orderRevenue.tHead)
+console.log(orderRevenue.tBodies)
+console.log(orderRevenue.tFoot)
