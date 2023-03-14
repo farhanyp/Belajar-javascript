ae07e8e7e35f73c00d22be0e4acb0220534e1fb4
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-03-14 12:18:07 +0700

    Ajax

---
+++ async/ajax/script.js
@@ -0,0 +1,11 @@
+// Json
+// Get Data Json
+
+const ajax = new XMLHttpRequest();
+ajax.open("GET", "ajax/hello.json")
+ajax.addEventListener('load', function(){
+    const json = JSON.parse(ajax.responseText)
+    const header = document.getElementById('respons')
+    header.textContent = json.respon
+})
+ajax.send()
