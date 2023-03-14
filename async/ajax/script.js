3b019daa7dec7488a881342a4bf7b818db532f4b
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-03-14 13:00:55 +0700

    Cek Status Ajax

--- async/ajax/script.js
+++ async/ajax/script.js
@@ -1,10 +1,32 @@
 // Json
 // Get Data Json
-const ajax = new XMLHttpRequest();
-ajax.open("GET", "ajax/hello.json")
-ajax.addEventListener('load', function(){
-    const json = JSON.parse(ajax.responseText)
-    const header = document.getElementById('respons')
-    header.textContent = json.respon
-})
-ajax.send()
+// const ajax = new XMLHttpRequest();
+// ajax.open("GET", "ajax/hello.json")
+// ajax.addEventListener('load', function(){
+//     const json = JSON.parse(ajax.responseText)
+//     const header = document.getElementById('respons')
+//     header.textContent = json.respon
+// })
+// ajax.send()
+
+
+// Cek status ajax
+// function displayRespons(json){
+//     const header = document.getElementById('respons')
+//     header.textContent = json.responn
+// }
+
+// const ajax = new XMLHttpRequest();
+// ajax.open("GET", "ajax/helllo.json")
+// ajax.addEventListener('load', function(){
+//     if(ajax.status == 200){
+//         const json = JSON.parse(ajax.responseText)
+//         displayRespons(json)
+//     }else{
+//         displayRespons({
+//             responn: `Terjadi kesalahan, status ${ajax.status}`
+//         })
+//     }
+
+// })
+// ajax.send()
